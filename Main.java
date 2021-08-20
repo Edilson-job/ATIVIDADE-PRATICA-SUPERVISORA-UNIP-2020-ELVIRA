1ecb9787f9276745454d3930cb3a1f488e1ba253
Edilson-job <89278995+Edilson-job@users.noreply.github.com>
2021-08-20 18:50:15 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+package com.edilson.aps.view;
+
+
+import com.edilson.aps.model.service.OrderingMethods;
+
+import java.sql.SQLException;
+
+public class Main {
+
+
+    public static void main(String[] args) throws SQLException {
+        Typesmenus.mainMenu();
+
+
+
+
+
+    }
+}
